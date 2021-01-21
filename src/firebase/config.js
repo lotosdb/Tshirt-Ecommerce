8c85288b4d2409b06831139bfe3d2cb4cf480af1
Andres Loto <riderandresj@hotmail.com>
2021-01-20 22:23:45 -0500

    Plantilla Ecommerce

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyA2-U5CVIZP71N39aWLVboFw1p0BcS7TDo",
+    authDomain: "tshirt-ecommerce-e0de1.firebaseapp.com",
+    projectId: "tshirt-ecommerce-e0de1",
+    storageBucket: "tshirt-ecommerce-e0de1.appspot.com",
+    messagingSenderId: "125947362933",
+    appId: "1:125947362933:web:5a5db9a60c5b5c9ab8423c"
+};
